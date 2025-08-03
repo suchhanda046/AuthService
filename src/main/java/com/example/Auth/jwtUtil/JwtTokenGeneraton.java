cfadb24a65a14d2c12cd0fd8f27c25f7b5de51be
suchhanda046 <49479310+suchhanda046@users.noreply.github.com>
2025-08-03 20:07:30 +0530

    atoring secret key in auth file

--- src/main/java/com/example/Auth/jwtUtil/JwtTokenGeneraton.java
+++ src/main/java/com/example/Auth/jwtUtil/JwtTokenGeneraton.java
@@ -1,18 +1,23 @@
 package com.example.Auth.jwtUtil;
 
 
+import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 import java.util.List;
 
 @Component
 public class JwtTokenGeneraton  {
-    private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    Dotenv dotenv = Dotenv.load();
+    String jwtKey = dotenv.get("JWT_SECRET");
+
+    private final SecretKey secretKey = Keys.hmacShaKeyFor(jwtKey.getBytes());
     private final long expireTime = 1000 * 60 * 60;
 
     public String generateToken(String username, List<String> roles){
